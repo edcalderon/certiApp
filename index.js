eccb1ffdde85cebd4e17a2f4b04083f1682b0642
edwardca12@gmail.com <edwardca12@gmail.com>
2019-12-18 12:51:36 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const { URLDB } = require('./config/config');
+const app = express();
+const path = require('path')
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 5000;
+const viewsDir = path.join(__dirname, './templates/views');
+
+require('./helpers/helpers');
+
+
+express()
+.use(bodyParser.urlencoded({extended: false}))
+.use(bodyParser.json())
+.use(express.static(path.join(__dirname, 'public')))
+.set('view engine', 'hbs')
+.set('views',viewsDir)
+
+.get('/',  (req, res) => {
+
+    res.render('index', {
+        list: certificates,
+        sort: req.query.sort
+    });
+})
+
+// mongoose Conection
+mongoose.connect(URLDB, { useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('moongose conected');
+	}
+});
