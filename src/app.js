4837a0423667bd6d50a336dbb88f2ee11700adc3
edcalderon <edwardca12@gmail.com>
2019-12-20 03:48:44 -0500

    mierda2.0

--- src/app.js
+++ src/app.js
@@ -17,7 +17,6 @@ app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
-
     if ('OPTIONS' == req.method) {
       res.sendStatus(200);
     }
