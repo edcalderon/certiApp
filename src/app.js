a37e04a349bec3001053c940ecccef2ef37d159b
edcalderon <edwardca12@gmail.com>
2019-12-20 03:46:24 -0500

    mierdaa

--- src/app.js
+++ src/app.js
@@ -1,45 +1,38 @@
-const { URLDB } = require('./config/config');
-const { PORT } = require('./config/config');
-const express = require('express');
+const express = require('express')
 const app = express();
-const path = require('path');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const hbs = require('hbs');
+const PORT = process.env.PORT || 5000
+const { URLDB } = require('./config/config.js')
+const mongoose = require('mongoose')
+const server = require('http').createServer(app);
+const bodyParser = require('body-parser')
+const path = require('path')
 const cors = require('cors')
 
-// Paths
-const directoryPublic = path.join(__dirname, '../public');
 
-//Cors
 app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, './../public')))
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header('Access-Control-Allow-Methods', 'GET');
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	if ('OPTIONS' == req.method) {
-	   res.sendStatus(200);
-	 }
-	 next();
- });
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-// Static
-app.use(express.static(directoryPublic));
-
-// BodyParser
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Routes
+    if ('OPTIONS' == req.method) {
+      res.sendStatus(200);
+    }
+	next();
+});
 app.use(require('./routes/index'));
 
-mongoose.connect(URLDB, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+server.listen(PORT, () => {
+	console.log(`Escuchando en el puerto ${PORT}`);
+});
+
+mongoose.connect(URLDB, { useNewUrlParser: true, useUnifiedTopology: true  }, (err) => {
 	if (err) {
 		console.log(err);
 	} else {
 		console.log('moongose conected');
 	}
-});
-
-
-app.listen(PORT, () => {
-	console.log(`Escuchando en el puerto ${PORT}`);
 });
