35831ac53cf8d9b2ae6f8116bdfcc444a09b4810
edcalderon <edwardca12@gmail.com>
2019-12-23 00:18:05 -0500

    final v1

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,6 +15,7 @@ app.set('view engine', 'hbs');// Le configuramos el motor de templates o de vist
 // Models mongodb
 const Certificates = require('./../models/certificate');
 
+
 app.get('/', cors(), (req, res) => {
     res.render('index', {
         message: "Bienvendio! para descargar tu certificado Ingresa tu documento en el recuadro",
@@ -22,18 +23,26 @@ app.get('/', cors(), (req, res) => {
     });
 });
 
-app.post('/', cors(), (req, res) => {
+
+app.get('/cicti', cors(), (req, res) => {
+    res.render('cicti', {
+        message: "Bienvendio! para descargar tu certificado Ingresa tu documento en el recuadro",
+        type: "info"
+    });
+});
+
+app.post('/cicti', cors(), (req, res) => {
     Certificates .findOne({ id: req.body.id }, (err, result) => {
         if (err) {
             console.log(err);
         }else if (result) {
-            res.render('index', {
+            res.render('cicti', {
                 certificates: result,
                 message: "Documento encontrado!, puedes descargar tu certificado dando click en DESCARGAR en la parte inferior ",
                 type: "success"
             });
         }else {
-            res.render('index', {
+            res.render('cicti', {
                 message: "Documento NO encontrado, ponte en contacto con soporte, Correo en la parte inferior",
                 type: "danger"
             });
@@ -41,20 +50,55 @@ app.post('/', cors(), (req, res) => {
     })
 });
 
-app.get('/certificate/:id',  (req, res) => {
-    if (req.params.id){
-        Certificates .findOne({ id: req.params.id }, (err, result) => {
-            if (err) {
-                console.log(err);
-            }else if (result) {
-                res.json(result);
-            }else {
-                res.json("Identificacion no encontrada ponte en contacto");
-            }
-        })
-    }else{
-        res.json("Ingresa un id");
-    }
-})
+
+app.get('/workshop', cors(), (req, res) => {
+    res.render('workshop', {
+        message: "Bienvendio! Ingresa una palabra presente en el inicio del titulo de tu articulo",
+        type: "info"
+    });
+});
+
+app.post('/workshop', cors(), (req, res) => {
+    console.log(req.body.id)
+    Certificates .findOne({ id: {$regex: req.body.id, $options: 'i'}}, (err, result) => {
+        console.log(result)
+        if (err) {
+            console.log(err);
+        }else if (result) {
+            res.render('workshop', {
+                certificates: result,
+                message: "encontrado!, puedes descargar tu certificado dando click en DESCARGAR en la parte inferior ",
+                type: "success"
+            });
+        }else {
+            res.render('workshop', {
+                message: "Documento NO encontrado, ponte en contacto con soporte, Correo en la parte inferior",
+                type: "danger"
+            });
+        }
+    })
+});
+
+
+app.get('/workshopfull', cors(), (req, res) => {
+    console.log(typeof req.body.id)
+    Certificates .find({}, (err, result) => {
+        console.log(result)
+        if (err) {
+            console.log(err);
+        }else if (result) {
+            res.render('workshop', {
+                certificates: result,
+                message: "encontrado!, puedes descargar tu certificado dando click en DESCARGAR en la parte inferior ",
+                type: "success"
+            });
+        }else {
+            res.render('workshop', {
+                message: "Documento NO encontrado, ponte en contacto con soporte, Correo en la parte inferior",
+                type: "danger"
+            });
+        }
+    })
+});
 
 module.exports = app;
